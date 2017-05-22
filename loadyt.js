7e83e252f5e512017eb5d07b2f3bff80bfbc6b91
Acc3ssViolation <Acc3ssViolation@users.noreply.github.com>
2017-05-22 19:38:51 +0200

    Update loadyt.js

--- loadyt.js
+++ loadyt.js
@@ -1,4 +1,9 @@
-window.onload = function()
+document.onload = function()
 {
-	 
+	var frame = document.getElementById("frame");
+	var links = ["TtHukQpboyk", "WK2siEQsADk", "AOai94e3MhE"];
+	var prefix = "https://www.youtube.com/embed/"
+	var postfix = "?autoplay=1"
+	var randomLink = links[Math.floor(Math.random() * links.length)];
+	frame.src = prefix + randomLink + postfix;
 }
