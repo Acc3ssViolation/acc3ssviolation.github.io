8dd815b0b3539a61f031c03b39478489ec02326f
Acc3ssViolation <Acc3ssViolation@users.noreply.github.com>
2018-01-18 22:31:10 +0100

    Adds playlist

--- loadyt.js
+++ loadyt.js
@@ -19,8 +19,56 @@ window.onload = function()
 		
 		
 	];
-	var prefix = "https://www.youtube.com/embed/"
-	var postfix = "?autoplay=1"
-	var randomLink = links[Math.floor(Math.random() * links.length)];
+	shuffle(links);
+	var index = 0;
+	
+	function getNextIndex() {
+		index += 1;
+		if(index >= links.length) {
+			shuffle(links);
+			index = 0;
+		}
+		return index;
+	}
+	
+	// create youtube player
+        var player;
+        function onYouTubePlayerAPIReady() {
+            player = new YT.Player('player', {
+              width: '0',
+              height: '0',
+              videoId: links[0],
+              events: {
+                onReady: onPlayerReady,
+                onStateChange: onPlayerStateChange
+              }
+            });
+        }
+	
+	function onPlayerReady(event) {
+		event.target.playVideo();
+	}
+	
+	function onPlayerStateChange(event) {
+		if(event.data === 0) {
+			var i = getNextIndex();
+			event.target.loadVideoById(links[i], 0, "default");
+		}
+		else if(event.data === 5) {
+			// cued
+			event.target.playVideo();
+		}
+	}
+	
+	function shuffle(a) {
+		var j, x, i;
+		for (i = a.length - 1; i > 0; i--) {
+			j = Math.floor(Math.random() * (i + 1));
+			x = a[i];
+			a[i] = a[j];
+			a[j] = x;
+		}
+	}
+	
 	frame.src = prefix + randomLink + postfix;
 }
